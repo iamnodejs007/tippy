4f7ed26331505109a7741f7d90cea67742dd7a90
Matt Johnson <matt@bamsas.com>
2017-08-09 16:28:19 -0700

    add buttons for changing tip value, custom tip field

--- App.js
+++ App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default class App extends React.Component {
   constructor(){
     super();
     this.state = {
-      inputValue: ''
+      inputValue: '',
+      tipPercentage: 0.2,
+    }
+  }
+
+  updateCustomTip(customTip){
+    if (customTip) {
+      this.setState({tipPercentage: parseFloat(customTip) / 100});
+    } else {
+      this.setState({tipPercentage: 0})
     }
   }
 
   render() {
     let tip = '0.00';
     if (this.state.inputValue){
-      tip = parseFloat(this.state.inputValue) * 0.2;
+      tip = parseFloat(this.state.inputValue) * this.state.tipPercentage;
       tip = (Math.round(tip * 100) / 100).toFixed(2);
     }
     return (
       <View style={styles.container}>
         <Text>
-          {tip}
+          ${tip}
         </Text>
         <TextInput
           value={this.state.inputValue}
@@ -26,8 +35,30 @@ export default class App extends React.Component {
           style={styles.input}
           placeholder='0.00'
           underlineColorAndroid={'transparent'}
-          onChangeText={(text) => this.setState({inputValue: text})}
+          onChangeText={text => this.setState({inputValue: text})}
         />
+        <View style={styles.buttonGroup}>
+          <Button
+            title="10%"
+            onPress={() => this.setState({tipPercentage: 0.1})}
+          />
+          <Button
+            title="15%"
+            onPress={() => this.setState({tipPercentage: 0.15})}
+          />
+          <Button
+            title="20%"
+            onPress={() => this.setState({tipPercentage: 0.2})}
+          />
+          <TextInput 
+            value={(this.state.tipPercentage * 100).toString()}
+            keyboardType='numeric'
+            placeholder='20%'
+            style={styles.customTip}
+            underlineColorAndroid={'transparent'}
+            onChangeText={customTip => this.updateCustomTip(customTip)}
+          />
+        </View>
       </View>
     );
   }
@@ -47,5 +78,15 @@ const styles = StyleSheet.create({
     borderColor: '#333',
     borderWidth: 1,
     padding: 5,
+  },
+  customTip: {
+    height: 30,
+    width: 60,
+    borderColor: '#333',
+    borderWidth: 1,
+    padding: 5,
+  },
+  buttonGroup: {
+    flexDirection: 'row'
   }
 });
